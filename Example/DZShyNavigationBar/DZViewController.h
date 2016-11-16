ffbe3aedf0c90693ab4fa04fd07c122d1d537c2c
yishuiliunian <yishuiliunian@gmail.com>
2016-11-16 12:52:19 +0800

    Initial commit

---
+++ Example/DZShyNavigationBar/DZViewController.h
@@ -0,0 +1,13 @@
+//
+//  DZViewController.h
+//  DZShyNavigationBar
+//
+//  Created by yishuiliunian on 11/16/2016.
+//  Copyright (c) 2016 yishuiliunian. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DZViewController : UIViewController
+
+@end
---
+++ Example/DZShyNavigationBar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DZShyNavigationBar
+//
+//  Created by yishuiliunian on 11/16/2016.
+//  Copyright (c) 2016 yishuiliunian. All rights reserved.
+//
+
+@import UIKit;
+#import "DZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DZAppDelegate class]));
+    }
+}
